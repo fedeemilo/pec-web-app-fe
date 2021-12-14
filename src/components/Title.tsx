664c50ae544d52fb58e1aca6aaeeddd87195c939
Federico Milone <federico.milone@claro.com.ar>
2021-12-14 16:13:48 -0300

    Add styled components and create Title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,9 +1,9 @@
 import styled from 'styled-components'
 
-const Index = styled('h1')<{color?: String}>`
+const Title = styled('h1')<{color?: String}>`
     
     color: ${props => props?.color ? props?.color : 'blue'}
 
 `
 
-export default Index
+export default Title
