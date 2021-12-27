d89564d2114fa849a93210e3de14cd41d165c6d6
Federico Milone <federico.milone@claro.com.ar>
2021-12-27 14:49:00 -0300

    update button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,16 @@
 import { useContext } from "react";
 import { CONTEXT } from "../context";
 
-import styled from 'styled-components'
+import styled from "styled-components";
 
-const ButtonUI = styled('button') <{ color?: String }>`
+const ButtonUI = styled("button")<{ color?: String }>`
     width: 100%;
     height: 3rem;
     background-color: #5eb8d2;
     color: #ffffff;
-`
+`;
 
 const Button = () => {
-
-
     return <ButtonUI>Buscar</ButtonUI>;
 };
 
