7311f039aa1a7aa61efb2531b1b7101be938f3b1
Federico Milone <federico.milone@claro.com.ar>
2022-01-17 14:32:36 -0300

    prepare for merge

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,12 +1,11 @@
-import { FunctionComponent, useRef } from "react"
-import { Container } from "../../Common/Grid/Index"
-import FormItem from "./FormItem"
-import { FormList, FormInterface } from "../../Types/formTypes"
-import { Dropdown } from "../../Common/UI"
-import { attributesHandler, formHandler } from "../../Utils/functions"
-import { FormContainer, Submit } from "./styles"
-import { elementAttrs } from "../../Mocks/UI/attributes"
-import { COLORS } from "../../Constants"
+import { FunctionComponent, useRef } from "react";
+import { Container } from "../../Common/Grid/Index";
+import FormItem from "./FormItem";
+import { FormList, FormInterface } from "../../Types/formTypes";
+import { Dropdown } from "../../Common/UI";
+import { formHandler } from "../../Utils/functions";
+import { FormContainer, Submit } from "./styles";
+import { COLORS } from "../../Constants";
 
 const Form: FunctionComponent<FormInterface> = ({
     list,
@@ -14,13 +13,13 @@ const Form: FunctionComponent<FormInterface> = ({
     callback,
     submitText
 }) => {
-    const formRef = useRef<HTMLFormElement>(null)
+    const formRef = useRef<HTMLFormElement>(null);
     const formDropdownStyles = {
         border: "1px solid #e0e0e0",
         padding: "13.68px 22px",
         color: COLORS.boldGray,
         bgColor: COLORS.smoothGray
-    }
+    };
 
     const renderFormElements = () =>
         list?.map((props: FormList, i: number) => {
@@ -35,25 +34,19 @@ const Form: FunctionComponent<FormInterface> = ({
                         isForm={true}
                         styles={formDropdownStyles}
                     />
-                )
+                );
 
-            return (
-                <FormItem
-                    {...props}
-                    key={i}
-                    colSize={6}
-                />
-            )
-        })
+            return <FormItem {...props} key={i} colSize={6} />;
+        });
 
     return (
         <FormContainer onSubmit={e => formHandler(e, callback)} ref={formRef}>
             <Container wrap={"wrap"}>
                 {renderFormElements()}
-                <Submit type={`submit`} value={submitText || "Enviar"} />
+                <Submit type={"submit"} value={submitText || "Enviar"} />
             </Container>
         </FormContainer>
-    )
-}
+    );
+};
 
-export default Form
+export default Form;
